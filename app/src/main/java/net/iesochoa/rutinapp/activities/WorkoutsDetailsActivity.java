1e2a7a86136cf3005e082437f79d54aac26e799e
Faus <fausz18@gmail.com>
2020-06-04 17:03:12 +0200

    Creación de WorkoutsDetailsActivity y rename de logo.

---
+++ app/src/main/java/net/iesochoa/rutinapp/activities/WorkoutsDetailsActivity.java
@@ -0,0 +1,16 @@
+package net.iesochoa.rutinapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.iesochoa.rutinapp.R;
+
+public class WorkoutsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts_details);
+    }
+}
